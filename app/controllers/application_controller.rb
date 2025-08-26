8f13c1b161fa46a5fe9645c548efc1382685fdc1
Abner Dascalakis <abnerdascalakis@gmail.com>
2025-08-26 10:35:42 -0400

    atualizaçaõ do pj1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # Redireciona depois do logout
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
-  
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  layout :set_layout
+
+  private
+
+  def set_layout
+    devise_controller? ? "devise_application" : "application"
+  end
 end
